4177616155803c5166d2eeb73f0406f567252a9c
Septimiu Draghici <septimiu.draghici@principal33.com>
2022-01-20 14:24:44 +0200

    Program takes 30-40s to load 100MB file to DB without parallel programming

---
+++ ImportCSVDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportCSVDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory;
+            string csvPath = path.Replace(@"bin\Debug", @"ArchiveData\customer_100MB.csv");
+            Console.WriteLine(csvPath);
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            var dataFromCSV = GetDataTabletFromCSVFile(csvPath);
+            using (SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=ImportCSV;Integrated Security=True"))
+            {
+                conn.Open();
+                using (SqlBulkCopy bulkCopy = new SqlBulkCopy(conn))
+                {
+                    bulkCopy.DestinationTableName = "dbo.customer";
+                    foreach (var column in dataFromCSV.Columns)
+                        bulkCopy.ColumnMappings.Add(column.ToString(), column.ToString());
+                    bulkCopy.WriteToServer(dataFromCSV);
+                }
+                conn.Close();
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Done importing customers!");
+            Console.WriteLine("Elapsed Time is {0} s", stopwatch.ElapsedMilliseconds/1000);
+            Console.ReadLine();
+        }
+        private static DataTable GetDataTabletFromCSVFile(string csv_file_path)
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(csv_file_path))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = true;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        //Making empty value as null
+                        for (int i = 0; i < fieldData.Length; i++)
+                        {
+                            if (fieldData[i] == "")
+                            {
+                                fieldData[i] = null;
+                            }
+                        }
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return csvData;
+        }
+    }
+}
